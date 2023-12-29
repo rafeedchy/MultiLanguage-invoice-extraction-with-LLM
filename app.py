b6358e08f775c7358d4c3c7719c53ee303c8f698
MD RAFEEDUL BAR CHOWDHURY <rafeedchy94@gmail.com>
2023-12-29 21:44:27 +0200

    app build!

--- app.py
+++ app.py
@@ -16,6 +16,21 @@ def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0], prompt])
     return response.text
 
+def input_image_details(uploaded_file):
+    if uploaded_file is not None:
+
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise Exception("Please upload an image")
+
 ## Initialize streamlit app
 
 st.set_page_config(page_title="Gemini Image Invoice Extractor")
@@ -31,3 +46,13 @@ if uploaded_file is not None:
 
 submit = st.button("Extract Invoice")
 
+input_prompt= """
+You are an expert at understanding invoices. We will upload an image as invoice and you will have to answere questions based on the uploaded invoice image. The language of the invoice could be non english but you will answer in English"""
+
+
+# if submit button is clicked
+if submit:
+    image_data = input_image_details(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data, input)
+    st.subheader("Gemini Response about the invoice: ")
+    st.write(response)
